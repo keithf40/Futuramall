ed1f2b00f1ebf6afc68ac74566eff0c9b6b79d19
kylucil <kyle.cortez2@gmail.com>
2025-07-30 19:51:30 -0400

    trying pick up function

---
+++ Futuramall/Assets/GRAB.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class ObjectGrabber : MonoBehaviour
+{
+public float grabDistance = 3f; // Maximum distance to grab objects
+public Transform holdPoint; // Point where the object will be held
+private GameObject grabbedObject;
+private Rigidbody grabbedRigidbody;
+
+void Update()
+{
+if (Input.GetMouseButtonDown(0)) // Left mouse button to grab/release
+{
+if (grabbedObject == null)
+{
+TryGrabObject();
+}
+else
+{
+ReleaseObject();
+}
+}
+
+if (grabbedObject != null)
+{
+// Keep the object at the hold point
+grabbedRigidbody.MovePosition(holdPoint.position);
+}
+}
+
+void TryGrabObject()
+{
+Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+if (Physics.Raycast(ray, out RaycastHit hit, grabDistance))
+{
+if (hit.collider.CompareTag("Grabbable")) // Ensure object has "Grabbable" tag
+{
+grabbedObject = hit.collider.gameObject;
+grabbedRigidbody = grabbedObject.GetComponent<Rigidbody>();
+
+if (grabbedRigidbody != null)
+{
+grabbedRigidbody.useGravity = false; // Disable gravity while holding
+grabbedRigidbody.freezeRotation = true; // Prevent rotation
+}
+}
+}
+}
+
+void ReleaseObject()
+{
+if (grabbedRigidbody != null)
+{
+grabbedRigidbody.useGravity = true;
+grabbedRigidbody.freezeRotation = false;
+}
+
+grabbedObject = null;
+grabbedRigidbody = null;
+}
+}
